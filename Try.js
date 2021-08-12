0a428cb7480e3b10b55200275b72d6bc30e32fe5
filipe yum <filipeyum@gmail.com>
2021-08-12 22:13:06 +0100

    First in a long line of tests.. Here lies my good will and my sanitude

---
+++ Try.js
@@ -0,0 +1,31 @@
+let dog = {
+    sound: 'woof',
+    talk: function(){
+        console.log(this.sound)
+    }
+}
+
+
+dog.talk() // "woof"
+
+let talkFunction = dog.talk 
+
+talkFunction() // undefined
+
+
+// but by binding we can make the this.binded to the function
+
+
+let boundFunction = talkFunction.bind(dog)
+boundFunction() // "woof"
+
+
+// some more shenanigans
+
+
+let button = document.getElementById('myNiceButton')
+
+button.addEventListener(
+    'click',
+    dog.talk.bind(dog) //again bind para ficar o 'woof'
+)
