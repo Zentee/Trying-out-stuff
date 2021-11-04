4d63f3e1b7b97a5e697c5a1f0f0e73ad6d3aac88
zentee <filipeyum@gmail.com>
2021-11-04 16:27:12 +0000

    big  upload of lots of things i worked on

---
+++ FrontEndMasters/Calculator Exercise 2/logic.js
@@ -0,0 +1,82 @@
+let total = 0;
+let buffer = "0";
+let previousOperator = null;
+const screen = document.querySelector(".resultscreen");
+
+document.querySelector(".buttons").addEventListener("click", function (event) {
+  buttonClick(event.target.innerText);
+});
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  reRender();
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      total = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + total;
+      total = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
+
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+  if (total === 0) {
+    total = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+
+  previousOperator = value;
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    total += intBuffer;
+  } else if (previousOperator === "-") {
+    total -= intBuffer;
+  } else if (previousOperator === "X") {
+    total *= intBuffer;
+  } else {
+    total /= intBuffer;
+  }
+}
+
+function reRender() {
+  screen.innerText = buffer;
+}
