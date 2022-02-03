d2cc3b54ace727de873a6f11fef1dcae9fdf9432
zentee <filipeyum@gmail.com>
2022-02-03 16:48:13 +0000

    More React Practice

---
+++ Beginner react course scrimba/First React/index.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+  <ol>
+    <li>first</li>
+    <li>second</li>
+  </ol>,
+  document.getElementById("first")
+);
+
+ReactDOM.render(<h1>PRIMEIRO</h1>, document.getElementById("root"));
